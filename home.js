c56c96be7e7049ad3e043b97d76c8bce758fd13a
LishadWahid <lishadwahid@gmail.com>
2025-08-29 01:24:28 +0300

    all new file are connected

--- home.js
+++ home.js
@@ -5,3 +5,39 @@ function increaseHeartCount() {
     count++;
     counter.innerText = count;
   }
+
+  function increaseCopyButton(){
+    const copyCounter = document.getElementById('countCopy');
+    let copy = parseInt(copyCounter.innerText);
+    copy++;
+    copyCounter.innerText = copy;
+  }
+
+
+
+
+  let coins = 100;
+
+  function getDateTime(){
+    const now = new Date();
+    return now.toLocaleString();
+  }
+  
+  document.getElementById('call1').addEventListener('click', function(e){
+    e.preventDefault()
+    const name = document.getElementById('name1').textContent;
+    const number = document.getElementById('number2').textContent;
+
+    if(coins < 20){
+        alert('Not enough coins to call');
+        return;
+    }
+
+    coins -= 20;
+    document.getElementById('coins').textContent = coins;
+    alert(`Calling ${name} at ${number}`);
+
+    const li = document.createElement('li');
+    li.textContent = `${name} - at ${number} (called at: ${getDateTime()})`;
+    document.getElementById('callHistory').appendChild(li)
+  })
